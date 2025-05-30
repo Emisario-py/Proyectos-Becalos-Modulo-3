61bd79622136e9281d211da201864c4e38dd7430
Emilio <EmilianoR117@outlook.com>
2025-05-29 22:29:42 -0700

    feat: Se agregaron comentarios al codigo de JavaScript del proyecto 1 para explicar su funcionamiento

--- Proyecto 1 (Event Loopp y Asincronicidad)/app.js
+++ Proyecto 1 (Event Loopp y Asincronicidad)/app.js
@@ -3,12 +3,14 @@ const addOrderBtn = document.getElementById('addOrderBtn');
 
 let orderId = 1; // Para identificar los pedidos
 
+// Aqui se utiliza un event listener para darle la funcion de agregar y procesar orden al boton de ordenar
 addOrderBtn.addEventListener('click', () => {
     const order = { id: orderId++, status: 'En Proceso' };
     addOrder(order);
     processOrder(order);
 });
 
+// Mediante esta funcion se añaden nuestros pedidos a nuestro html y por ende a nuestra pagina
 function addOrder(order) {
     const listItem = document.createElement('li');
     listItem.id = `order-${order.id}`;
@@ -16,6 +18,7 @@ function addOrder(order) {
     orderList.appendChild(listItem);
 }
 
+// En esta funcion se busca actualizar el status de nuestro pedido
 function updateOrderStatus(order, status) {
     const listItem = document.getElementById(`order-${order.id}`);
     if (listItem) {
@@ -24,11 +27,11 @@ function updateOrderStatus(order, status) {
 }
 
 async function processOrder(order) {
-    // TODO: Simular la preparación del pedido usando setTimeout y Promise
+    // Aqui se crea una promesa y se utiliza un setTimeout para simular la duracion del pedido
     const promesa = new Promise((resolve) => {
         setTimeout(() => resolve('Pedido completado'),  Math.floor(Math.random() * 5000) + 1000);
     })
-    // TODO: Actualizar el estado del pedido a "Completado"
+    // Aqui se actualiza el estado del pedido a "Completado" utilizando una promesa, la idea es que nuevostatus se actualize una vez la promesa se ha completado
     const nuevostatus = await promesa
     updateOrderStatus(order, nuevostatus);
 }
