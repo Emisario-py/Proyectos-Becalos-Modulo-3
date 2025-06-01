e8801c4af8f969eb21b917e92069a18f5bb34b68
Emilio <EmilianoR117@outlook.com>
2025-06-01 01:19:16 -0700

    feat: Se añadio el taller de node.js

---
+++ mi-exploracion-espacial/planetas.js
@@ -0,0 +1,40 @@
+const planetas = [
+  {
+    nombre: "Titán",
+    descripcion: "La luna más grande de Saturno, con lagos de metano.",
+    descubiertoEn: "1655"
+  },
+  {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+    {
+    nombre: "Próxima Centauri b",
+    descripcion: "Un exoplaneta rocoso en la zona habitable de su estrella.",
+    descubiertoEn: "2016"
+  },
+  {
+    nombre: "Kepler-186f",
+    descripcion: "El primer exoplaneta de tamaño similar a la Tierra descubierto en la zona habitable de otra estrella.",
+    descubiertoEn: "2014"
+  },
+  {
+    nombre: "TRAPPIST-1e",
+    descripcion: "Uno de los siete planetas del sistema TRAPPIST-1, potencialmente habitable.",
+    descubiertoEn: "2017"
+  },
+  {
+    nombre: "Gliese 581g",
+    descripcion: "Un candidato a planeta habitable en el sistema Gliese 581, aunque su existencia es debatida.",
+    descubiertoEn: "2010"
+  },
+  {
+    nombre: "HD 209458 b",
+    descripcion: "El primer exoplaneta observado en tránsito, también conocido como Osiris.",
+    descubiertoEn: "1999"
+  }
+  // ¡Añade más planetas!
+];
+
+module.exports = planetas;
